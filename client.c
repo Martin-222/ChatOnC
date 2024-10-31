b8be3849a96693539fdac669f43fc5ae1a215185
Martin-222 <185973025+Martin-222@users.noreply.github.com>
2024-11-01 00:00:00 +0500

    'add file client.c'

---
+++ client.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "client.h"
+#include "utils.h"
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void start_client() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return;
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr,
